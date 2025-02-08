647c1326bb3725f3ec3708fd8d6014bce395814b
gleidson fagno <gleidsonfagner838@gmail.com>
2025-02-08 08:56:08 -0300

    Versionando as urls da api

--- app/urls.py
+++ app/urls.py
@@ -1,19 +1,22 @@
 
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListAPIView, GenreRetrieveUpdateDestroyView
+from django.urls import path, include
+
 from actors.views import ActorCreateListView,  ActorRetriveUpdatDestroyView
 from movies.views import MovieCreateLisView, MovieRetrieveUpdateDestroyView
+from reviews.views import ReviewCreateListView, ReviewRetriveUpdateDestroy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('genres/', GenreCreateListAPIView.as_view(), name="genre-create-list"),
-    path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(), name="genre-detail-view"),
+    path("api/v1/", include("genres.urls")),
 
     path("actors/", ActorCreateListView.as_view(), name="actors-create-list"),
     path("actors/<int:pk>", ActorRetriveUpdatDestroyView.as_view(), name="actors-detail-view"),
 
     path("movies/", MovieCreateLisView.as_view(), name="movie-create-list"),
     path("movies/<int:pk>", MovieRetrieveUpdateDestroyView.as_view(), name="movie-detail-view"),
+
+    path("reviews/", ReviewCreateListView.as_view(), name="review-create-list"),
+    path("reviews/<int:pk>", ReviewRetriveUpdateDestroy.as_view(), name="review-detail-view"),
 ]
--- reviews/models.py
+++ reviews/models.py
@@ -13,6 +13,5 @@ class Review(models.Model):
     )
     comment = models.TextField(null=True, blank=True)
 
-
     def __str__(self):
-        return self.movie
+        return str(self.movie)
